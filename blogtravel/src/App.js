ce2a617c8786325c60e53f4cefa623687150232f
VINIshow1907 <viniciuslucio792@gmail.com>
2024-04-10 13:24:58 -0300

    Navbar e o footer sendo criados

--- blogtravel/src/App.js
+++ blogtravel/src/App.js
@@ -1,28 +1,24 @@
 import "./App.css";
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-function App() { 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
 
-  return ( 
-    <div className="App"> 
-      <BrowserRouter> 
-        <div className="container"> 
-          <Routes> 
-            <Route path="/" element={<Home />} /> 
-            <Route path="/about" element={<About />} />  
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
 
-          </Routes> 
-        </div> 
-        
-      </BrowserRouter> 
-    </div> 
-  );  
-
-} 
-
-
-export default App; 
-
- 
+export default App;
