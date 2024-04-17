a13fe70230ea4ea0e8736ce1131b4cce557789aa
VINIshow1907 <viniciuslucio792@gmail.com>
2024-04-17 14:16:26 -0300

    Adicionei o componente fFOOTER

--- blogtravel/src/App.js
+++ blogtravel/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
@@ -9,13 +10,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
