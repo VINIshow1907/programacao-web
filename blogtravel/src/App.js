fd92b3c4839cfe8ba5eaae817fe3e62597c4c8b3
VINIshow1907 <viniciuslucio792@gmail.com>
2024-04-24 14:34:50 -0300

    Importei as paginas de login e register

--- blogtravel/src/App.js
+++ blogtravel/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Routes>
         </div>
         <Footer/>
