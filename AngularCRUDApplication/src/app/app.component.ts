9718a525c0a02270cb648841f3f79ccb1726d6a7
VINIshow1907 <viniciuslucio792@gmail.com>
2024-10-28 15:25:55 -0300

    :construction: Criando e arrumando as páginas

--- AngularCRUDApplication/src/app/app.component.ts
+++ AngularCRUDApplication/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Router } from '@angular/router';
+
+
 
 @Component({
+
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+
+  styleUrls: ['./app.component.scss']
+
 })
+
 export class AppComponent {
-  title = 'AngularCRUDApplication';
-}
+
+
+
+  constructor(private router: Router) { }
+
+
+
+  HomeClick() {
+
+    this.router.navigate(['Home']);
+
+  }
+
+} 
--- AngularCRUDApplication/src/app/view-employee/view-employee.component.ts
+++ AngularCRUDApplication/src/app/view-employee/view-employee.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-view-employee',
-  standalone: true,
-  imports: [],
   templateUrl: './view-employee.component.html',
   styleUrl: './view-employee.component.scss'
 })
